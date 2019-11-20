02bd4d1521fe2df6db546bd8af179b0768028ae9
ankomahene <ankomaheneshadrack@gmail.com>
2019-11-20 19:12:42 +0000

    init

---
+++ src/main/java/io/turntabl/tcmsEmail/TcmsEmailApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.tcmsEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmsEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcmsEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/tcmsEmail/TcmsEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.tcmsEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcmsEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
